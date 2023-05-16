1d05762b1a79c5bc934f598c65d67bb33b45c490
Ridho <alfaniridho2@gmail.com>
2023-05-16 21:57:39 +0700

    Initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="about">
+    <h1>السلام عليكم ورحمة الله وبركاته</h1>
+
+    <h2><img src="../assets/bismillah.png"></h2>
+
+    <h3>Arti Alquran menurut bahasa berasal dari kata kerja qaraa yang berarti: “(dia) telah membaca”.
+      Dari pengertian itu maka Quran berarti “bacaan” atau “sesuatu yang dibaca dengan berulang-ulang”.
+      Makna Quran dari segi bahasa tersebut didasarkan pada firman Allah dalam Alquran surat Al-Qiyamah ayat 16-18:
+
+      “Janganlah kamu gerakkan lidahmu untuk membaca Alquran karena hendak cepat-cepat (menguasai)nya.
+      Sesungguhnya atas tanggungan Kami-lah mengumpulkannya (di dadamu) dan (membuatmu pandai) membacanya.
+      Apabila Kami telah selesai membacakannya maka ikutilah bacaannya itu”.</h3>
+
+    <h4>Qur’an  diartikan  sebagai “bacaan”, yakni kalam Allah yang dibaca dengan berulang-ulang.
+        Adapun definisi Alquran secara istilah, Muhammad ‘Ali ash-Shabuni menulisnya sebagai berikut:
+      “Alquran adalah kalam Allah yang tiada tandingannya, diturunkan kepada Nabi Muhammad SAW penutup para Nabi dan Rasul,
+       dengan perantara Malaikat Jibril ‘alaihissalam dan ditulis pada mushaf-mushaf yang kemudian disampaikan kepada kita secara mutawatir,
+       serta membaca dan mempelajarinya merupakan suatu ibadah, yang dimulai dengan surat Al-Fatihah dan ditutup dengan surat An-Nas”.
+       Alquran sebagai sesuatu yang tiada tandingannya, dapat ditemukan dalam surat Al-Isra’ ayat 88:
+      “Katakanlah: ‘Sesungguhnya jika manusia dan jin berkumpul untuk berbuat yang serupa Alquran ini niscaya mereka tidak akan dapat membuat yang serupa dengan dia,
+       sekalipun sebagian mereka menjadi pembantu bagi sebagian yang lain.’”
+       Alquran sebagai wahyu yang diturunkan oleh Allah ke dalam hati nabi Muhammad SAW melalui Malaikat Jibril,
+       dapat ditemukan dalam surat Asy-Syu’ara ayat 192-194:
+       “Dan sesungguhnya Alquran ini benar-benar diturunkan oleh Tuhan semesta alam.
+       Dia dibawa turun oleh Ar-Ruh Al-Amin (Jibril) ke dalam hatimu (Muhammad) agar kau menjadi salah seorang di antara orang-orang yang memberi peringatan”.
+       Jaminan balasan dari Allah bagi orang-orang yang membaca dan mempelajari Qur’an,
+       disebutkan dalam surat Al-Isra’ ayat 45 sebagai berikut:
+      “Dan apabila kamu membaca Alquran niscaya kami adakan antara kamu dan orang-orang yang tidak beriman kepada kehidupan akhirat,
+       suatu dinding yang tertutup”
+       Pemberlakuan nama “Qur’an” bagi keseluruhan kandungan mushaf Qur’an didasarkan kepada firman Allah dalam surat Al-Isra’ ayat 9 sebagai berikut:
+      “Sesungguhnya Alquran ini memberikan petunjuk kepada (jalan) yang lebih lurus dan memberi kabar gembira kepada orang-orang Mu’min yang mengerjakan amal shaleh bahwa bagi mereka ada pahala yang besar”.</h4>
+  </div>
+</template>
+
+<style>
+
+</style>
+
